380659c961d977a2d3e11c199559e47840b3e1ba
Roaim <roimkhan459@gmail.com>
2026-08-26 09:46:06 +0500

    chore:commit trivial chores

--- data/database_models.py
+++ data/database_models.py
@@ -8,21 +8,17 @@ engine = create_engine("postgresql+psycopg2://postgres:mysecretpassword@localhos
 
 class Base(DeclarativeBase):
     pass
+
 class AssignmentDB(Base):
     __tablename__= "assignments"
     id: Mapped[str] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column()
     courseId: Mapped[str] = mapped_column()
     description: Mapped[Optional[str]] = mapped_column(nullable=True)
-# Change Optional[str] -> dict / list since they are JSON columns
     driveId: Mapped[Optional[dict]] = mapped_column(JSON, nullable=True)
     materials: Mapped[Optional[list]] = mapped_column(JSON, nullable=True)
-
-    # Use datetime instead of date if your due dates include hours/minutes (e.g., 2026-08-04 18:59)
     dueDate: Mapped[Optional[datetime]] = mapped_column(nullable=True)
 
 AssignmentDB.metadata.create_all(bind=engine)
-
 SessionLocal = sessionmaker(bind=engine)
-
 session = SessionLocal()
