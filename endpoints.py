4068f269caffa1770f021562232b09a7c1d1e362
Roaim <roimkhan459@gmail.com>
2026-08-28 08:37:13 +0500

    feat(api): add pending assignments endpoint

--- endpoints.py
+++ endpoints.py
@@ -1,13 +1,17 @@
 from gc_agent.dir import DATA_DIR
 from gc_agent.fetcher.fetcher import build_fetcher
 from gc_agent.data.database_models import engine, init_db
-from gc_agent.data.database_ops import updateAssgnDb, getPendingAssgnCountFrmDb, _writetodb
+from gc_agent.data.database_ops import updateAssgnDb, getPendingAssgnCountFrmDb, _writetodb, getPendingAssgnFrmDb
 import asyncio
 import json
 from pprint import pprint
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 path = DATA_DIR / "registered_courses.json"
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 @asynccontextmanager
@@ -18,6 +22,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
+
 @app.get("/getPendingassgn")
 async def getPendingAssgn():
     await updateAssgnDb()
@@ -25,6 +36,12 @@ async def getPendingAssgn():
     print(assgn)
     return {"total_pending_assgn": assgn}
 
+@app.get("/fetchPendingAssignments")
+async def fetchPendingAssignments():
+    await updateAssgnDb()
+    assgn = await getPendingAssgnFrmDb()
+    print(assgn)
+    return {"all_pending_assgn": assgn}
 
 if __name__ == "__main__":
     fetcher = build_fetcher()
